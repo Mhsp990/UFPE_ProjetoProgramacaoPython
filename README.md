e542acb0518c8e3e975c584404701aa918f8d4be
matheus <souza.passos990@gmail.com>
2026-09-09 22:03:22 -0300

    rota direta initial implementation

--- README.md
+++ README.md
@@ -16,4 +16,5 @@ OBS : Estou supondo que basta adicionar o item coletado via sucção em bandeja 
 
 
 ESTRATÉGIAS
+Para as estrategias funcionarem, decidi que elas precisam receber, por padrão, a localização que devem alcançar.
 Rota direta : Vai direto até uma pratileira (coordenada).
--- celular_robo/src/celular_robo/estrategias.py
+++ celular_robo/src/celular_robo/estrategias.py
@@ -8,8 +8,13 @@
 # aplicada a uma segunda hierarquia).
 
 from abc import ABC, abstractmethod
+from celular_robo.robo_base import Direcao
+
 
 class RotaColeta(ABC):
+    posicao_alvo_x = 0
+    posicao_alvo_y = 0
+
     _registro_rotas = {}
 
     def __init_subclass__(cls, **kwargs):
@@ -17,7 +22,9 @@ class RotaColeta(ABC):
         RotaColeta._registro_rotas[cls.__name__] = cls
 
 
-    def __init__(self):
+    def __init__(self, alvo_x, alvo_y):
+        self.posicao_alvo_x = alvo_x
+        self.posicao_alvo_y = alvo_y
         pass
 
     @abstractmethod   
@@ -27,8 +34,65 @@ class RotaColeta(ABC):
 
 class RotaDireta(RotaColeta):
     #Vai direto até cada pratileira.
-    #
-    pass
+    def mover(self, robo):
+
+        alvo = (self.posicao_alvo_x, self.posicao_alvo_y)
+
+        num_movimentacoes = 0
+        max_movimentacoes = 100 #Evitar loops infinitos.
+
+
+        while robo.posicao != alvo and (num_movimentacoes < max_movimentacoes):
+            direcao = self._direcao_para_alvo(robo)
+            robo.girar_ate(direcao)
+
+            num_movimentacoes += 1
+            if robo.avancar():
+                #Se conseguiu avançar, passe para a próxima iteração.
+                continue
+            else:
+                tentativas = 0
+                while not robo.sensor_frente() and tentativas < 4:
+                    #TODO : Mudar isso para EVITAR a direção oposta a inicial, para evitar "vai e vem" pro mesmo lugar.
+                    #Provavelmente deve ser possível ao restringir a apenas um giro para a esquerda e, depois, dois para a direita.
+                    robo.girar("DIR")
+                    tentativas += 1
+            
+
+
+        if num_movimentacoes < max_movimentacoes:
+            print("Rota direta falhou em mover-se ate o alvo antes do maximo")
+            return False
+        else:
+            print("Rota direta conseguiu mover-se até o alvo antes do maximo de movimentacoes.")
+            print(f"Num de movimentacoes necessarias: {num_movimentacoes}")
+            return True
+
+
+        
+
+
+
+
+
+
+
+    def _direcao_para_alvo(self, robo):
+    #Verifica qual direcao deve girar para ir até a posição alvo.
+        if robo.x < self.posicao_alvo_x:
+            return Direcao.LESTE
+
+        if robo.x > self.posicao_alvo_x:
+            return Direcao.OESTE    
+
+        if robo.y < self.posicao_alvo_y:
+            return Direcao.NORTE
+
+        return Direcao.SUL
+
+
+
 
 class RotaComDuplaConferencia(RotaColeta):
+    #
     pass
